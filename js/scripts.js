7b0a70e712c76958a05ca62a243cd91343a8ca5d
MungaiKeren <wambukeren@gmail.com>
2019-08-04 10:09:21 +0300

    This commit will add functionality to the form

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('form#form').submit(function(){
+        $('form#form').hide();
+        $('#p').text("We have recieved your response thank you for reaching out to us!")
+    })
+})
